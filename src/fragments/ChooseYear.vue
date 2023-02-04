eec642f14eaf24d1c7f309d49e156725caa59290
Himmelbleu <zhengrenfu@outlook.com>
2023-02-04 17:52:09 +0800

    :heavy-minus-sign: 删除错误代码

--- src/fragments/ChooseYear.vue
+++ src/fragments/ChooseYear.vue
@@ -12,39 +12,7 @@ const props = defineProps({
 
 const picker = ref([new Date(new Date().setMonth(0)), new Date(new Date().setMonth(11))]);
 
-props.bills.forEach(bill => bill.show && (bill.show = false));
-search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], Number(useDateFormat(useNow(), "YYYY").value));
-
-function search(storage: number[], year: number) {
-  for (let index = 0; index < props.bills.length; index++) {
-    const mont = storage.find(el => el == props.bills[index].month);
-    const bill = props.bills.find(el => el.month == mont && el.year == year);
-    bill && (bill.show = true);
-  }
-}
-
-function change(val: any) {
-  const sDate = useDateFormat(val[0], "YYYY-MM").value.split("-");
-  const sYear = Number(sDate[0]);
-  const sMonth = Number(sDate[1]);
-  const eDate = useDateFormat(val[1], "YYYY-MM").value.split("-");
-  const eYear = Number(eDate[0]);
-  const eMonth = Number(eDate[1]);
-  const sStore: number[] = [];
-  const eStore: number[] = [];
-
-  props.bills.forEach(bill => bill.show && (bill.show = false));
-
-  if (sYear === eYear) {
-    for (let i = sMonth; i <= eMonth; i++) sStore.push(i);
-    search(sStore, sYear);
-  } else {
-    for (let i = sMonth; i <= 12; i++) sStore.push(i);
-    for (let i = 1; i <= eMonth; i++) eStore.push(i);
-    search(sStore, sYear);
-    search(eStore, eYear);
-  }
-}
+function change(val: any) {}
 </script>
 
 <template>
