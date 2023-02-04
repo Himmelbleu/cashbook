1428a44471df4d793be2211d3996c5b54fb8bd57
Himmelbleu <zhengrenfu@outlook.com>
2023-02-05 02:30:25 +0800

    :lipstick: 修复菜单栏背景色

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@ const show = ref(false);
 
 <template>
   <div class="header f-c-b">
-    <TextIcon icon="menu" @click="show = !show" :icon-size="2" :text-size="1" />
+    <TextIcon icon="operation" @click="show = !show" :icon-size="2" :text-size="1" />
     <div class="fsz-1.4">{{ cabinetVal.name }} 的账单</div>
   </div>
   <BillMain />
   <div class="cabinet absolute top-0 left-0" :class="{ 'z-99': show, 'z--1': !show }">
     <div class="relative">
-      <div class="content p-4 w-60vw absolute top-0 left-0" :class="{ bg: show }">
+      <div class="content bg p-4 w-60vw absolute top-0 left-0" :class="{ bg: show }">
         <CabinetContent />
       </div>
       <div @click="show = !show" class="modal w-40vw absolute top-0 left-60vw"></div>
